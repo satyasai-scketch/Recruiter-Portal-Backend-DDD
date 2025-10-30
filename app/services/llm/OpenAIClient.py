afe10797007144249da44a4554ed211ad749b1ec
krishnaugale <krishna.ugale@altuminglobal.com>
2025-10-30 18:24:09 +0530

    persona api datetime added

--- app/services/llm/OpenAIClient.py
+++ app/services/llm/OpenAIClient.py
@@ -10,12 +10,13 @@ class OpenAIClient(LLMChatClient):
         self.client = wrap_openai(AsyncOpenAI(api_key=api_key))
         #self.model = model
     
-    async def chat_completion(self, messages: List[Dict[str, Any]],model: str = "gpt-4o-mini", temperature: float = 0.7, max_tokens: int = 2000,response_format: Dict[str, Any] = None) -> Dict[str, Any]:
+    async def chat_completion(self, messages: List[Dict[str, Any]],model: str = "gpt-4o-mini", temperature: float = 0.7, max_tokens: int = 5000,response_format: Dict[str, Any] = None) -> Dict[str, Any]:
         """Generate a chat completion"""
         response = await self.client.chat.completions.create(model=model, messages=messages, temperature= temperature, max_tokens=max_tokens,response_format=response_format)
         return response
 
     def get_model_info(self) -> Dict[str, Any]:
+        
         return {
             'provider': 'openai',
             'model': self.model,
